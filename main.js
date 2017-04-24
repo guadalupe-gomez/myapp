50979388bd6d96385ac0a322cf4259a198203854
guadalupe-gomez <guada.sgomez@gmail.com>
2017-04-24 17:49:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,18 @@
-//var http = require("http");
+var http = require("http");
 
-//http.createServer(function (request, response) {
+http.createServer(function (request, response) {
    // Send the HTTP header 
    // HTTP Status: 200 : OK
    // Content Type: text/plain
-   //response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   //response.end('Hello World Modificado \n');
-//}).listen(5000);
+   response.end('Hello World Modificado \n');
+}).listen(8080);
 
 // Console will print the message
 //console.log('Server running at http://127.0.0.1:8080/');
-//console.log("MyAPP-24/04/2017")
+console.log("MyAPP-24/04/2017")
 
-var express = require('express');
-var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
 
